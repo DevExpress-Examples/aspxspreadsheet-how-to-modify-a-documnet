b1943784bcb8c66cac8b6cdf8fe944846c84b6cc
natashaash <natalya.shirokopoyas@devexpress.com>
2019-06-18 16:10:39 +0300

    example content

---
+++ aspxspreadsheet-how-to-modify-a-documnet/CS/DXWebApplication1/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DXWebApplication1.WebForm1" %>
+
+<%@ Register Assembly="DevExpress.Web.ASPxSpreadsheet.v18.2, Version=18.2.8.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.Web.ASPxSpreadsheet" TagPrefix="dx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script>
+        function OnCommandExecuted(s, e) {
+            var command = e.item.name;
+            ASPxSpreadsheet1.PerformCallback(command)
+        }
+
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <dx:ASPxRibbon runat="server" ID="ASPxRibbon1" ShowFileTab="false" ShowTabs="false" OneLineMode="true">
+                <ClientSideEvents CommandExecuted="OnCommandExecuted" />
+                <Tabs>
+                    <dx:RibbonTab>
+                        <Groups>
+                            <dx:RibbonGroup  >
+                                <Items>
+                                    <dx:RibbonButtonItem Text="Apply formatting" Name="applyFormatting"></dx:RibbonButtonItem>
+                                    <dx:RibbonButtonItem Text="Insert link" Name="insertLink"></dx:RibbonButtonItem>
+                                    <dx:RibbonButtonItem Text="Draw Borders" Name="drawBorders"></dx:RibbonButtonItem>
+                                    <dx:RibbonButtonItem Text="Show total" Name="showTotal"></dx:RibbonButtonItem>
+                                </Items>
+                            </dx:RibbonGroup>
+                        </Groups>
+                    </dx:RibbonTab>
+                </Tabs>
+            </dx:ASPxRibbon>
+            <dx:ASPxSpreadsheet runat="server" ID="ASPxSpreadsheet1" ClientInstanceName="ASPxSpreadsheet1" OnCallback="ASPxSpreadsheet1_Callback"></dx:ASPxSpreadsheet>
+        </div>
+    </form>
+</body>
+</html>
---
+++ aspxspreadsheet-how-to-modify-a-documnet/CS/DXWebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,56 @@
+﻿using DevExpress.Spreadsheet;
+using DevExpress.Web.ASPxSpreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DXWebApplication1 {
+    public partial class WebForm1 : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            ASPxSpreadsheet1.Open(MapPath("~/Docs/testDocument1.xlsx"));
+        }
+
+        protected void ASPxSpreadsheet1_Callback(object sender, DevExpress.Web.CallbackEventArgsBase e) {
+            ASPxSpreadsheet spreadSheet = sender as ASPxSpreadsheet;
+            IWorkbook workbook = spreadSheet.Document;
+            Worksheet worksheet = workbook.Worksheets[0];
+            switch (e.Parameter) {
+                case "applyFormatting":
+                    Range priceRange = worksheet.Range["C2:C15"];
+                    Formatting rangeFormatting = priceRange.BeginUpdateFormatting();
+                    rangeFormatting.Font.Color = Color.SandyBrown;
+                    rangeFormatting.Font.FontStyle = SpreadsheetFontStyle.Bold;
+                    rangeFormatting.Fill.BackgroundColor = Color.PaleGoldenrod;
+                    rangeFormatting.NumberFormat = "$0.0#";
+
+                    rangeFormatting.Alignment.Vertical = SpreadsheetVerticalAlignment.Center;
+                    rangeFormatting.Alignment.Horizontal = SpreadsheetHorizontalAlignment.Center;
+                    priceRange.EndUpdateFormatting(rangeFormatting);
+                    break;
+                case "insertLink":
+                    worksheet.Columns["G"].WidthInPixels = 180;
+                    Cell cell1 = worksheet.Cells["G4"];
+                    cell1.Fill.BackgroundColor = Color.WhiteSmoke;
+                    worksheet.Hyperlinks.Add(cell1, "https://documentation.devexpress.com/OfficeFileAPI/14912/Spreadsheet-Document-API", true, "Spreadsheet Document API");
+                    break;
+                case "drawBorders":
+                    Range tableRange = worksheet.Range["A2:E16"];
+                    tableRange.Borders.SetAllBorders(Color.RosyBrown, BorderLineStyle.Hair);
+                    break;
+                case "showTotal":
+                    Cell cell2 = worksheet.Cells["E16"];
+                    cell2.Formula = "=SUBTOTAL(9,E2:E15)";
+                    Cell cell3 = worksheet.Cells["A16"];
+                    cell3.Formula = "SUBTOTAL(103,A2:A15)";
+                    Cell cell4 = worksheet.Cells["D16"];
+                    cell4.Value = "Total amount";
+                    break;
+            }
+
+        }
+    }
+}
